9b22922fb9db1e39de34f7dca45500fc05bf218a
Mark Wainwright <wainwrightml@gmail.com>
2023-05-19 16:38:33 +0100

    added logo watermark

--- netlify/functions/image/src/main.rs
+++ netlify/functions/image/src/main.rs
@@ -2,8 +2,8 @@ pub mod color;
 pub mod encoding;
 pub mod fixed_shape;
 pub mod game_shape;
-pub mod screenshots;
 pub mod point;
+pub mod svg;
 
 use aws_lambda_events::encodings::Body;
 use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
@@ -11,8 +11,9 @@ use base64::Engine;
 use http::header::HeaderMap;
 use http::HeaderValue;
 use lambda_runtime::{service_fn, Error, LambdaEvent};
+use resvg::tiny_skia::Transform;
 use resvg::usvg::{AspectRatio, NodeExt, PathBbox, Tree, TreeParsing, ViewBox};
-
+use resvg::FitTo;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -54,27 +55,32 @@ pub(crate) async fn my_handler(
     Ok(resp)
 }
 
-fn make_svg_text(data: &str) -> String {
+fn make_svg_from_data(data: &str) -> String {
     let Ok(bytes) = base64::engine::general_purpose::URL_SAFE.decode(data) else {return "".to_string();};
 
     let shapes = encoding::decode_shapes(&bytes);
 
-    let svg_text = screenshots::create_svg(shapes.into_iter());
-    svg_text
+    let svg = svg::create_svg(shapes.into_iter());
+    svg
 }
 
 fn draw_image(game: &str) -> Vec<u8> {
     let opt: resvg::usvg::Options = Default::default();
-    let svg_data = make_svg_text(game);
+    let svg_data = make_svg_from_data(game);
 
-    //println!("{svg_data}");
-
-    let mut tree = match Tree::from_data(&svg_data.as_bytes(), &opt) {
+    let mut game_tree = match Tree::from_data(&svg_data.as_bytes(), &opt) {
         Ok(tree) => tree,
         Err(e) => panic!("{e}"),
     };
 
-    let bbox = tree
+    let logo_bytes = include_bytes!("logo_monochrome.svg");
+
+    let logo_tree = match Tree::from_data(logo_bytes, &opt) {
+        Ok(tree) => tree,
+        Err(e) => panic!("{e}"),
+    };
+
+    let bbox = game_tree
         .root
         .calculate_bbox()
         .unwrap_or(PathBbox::new(0., 0., WIDTH as f64, HEIGHT as f64).unwrap());
@@ -91,7 +97,7 @@ fn draw_image(game: &str) -> Vec<u8> {
 
     let length_to_use = (bbox_size.width().max(bbox_size.height()) as f64) * 1.1;
 
-    tree.view_box = ViewBox {
+    game_tree.view_box = ViewBox {
         rect: resvg::usvg::Rect::new(
             bbox.x() - ((length_to_use - bbox.width()) * 0.75),
             bbox.y() - ((length_to_use - bbox.height()) * 0.75),
@@ -106,23 +112,31 @@ fn draw_image(game: &str) -> Vec<u8> {
         },
     };
 
-    use resvg::FitTo;
     resvg::render(
-        &tree,
+        &game_tree,
         FitTo::Size(RESOLUTION, RESOLUTION),
         Default::default(),
         pixmap.as_mut(),
     )
-    .unwrap();
+    .expect("Could not render game svg");
 
-    pixmap.encode_png().unwrap()
+    resvg::render(
+        &logo_tree,
+        FitTo::Size(RESOLUTION, RESOLUTION),
+        Transform::from_translate(0.0, 200.0),
+        pixmap.as_mut(),
+    )
+    .expect("Could not render logo svg");
+
+    pixmap.encode_png().expect("Could not encode png")
 }
 
 #[cfg(test)]
 mod tests {
 
-    use crate::{draw_image, make_svg_text};
-    const TEST_DATA: &'static str = "EHWEbQIBEIBBdrntBIM1ZLTwA38jYMQeHoeAaM12CHQ3ctMBBoijcwmyCIfGbUV2EHRSZmp5AoA9fGKT";
+    use crate::{draw_image, make_svg_from_data};
+    const TEST_DATA: &'static str =
+        "EHWEbQIBEIBBdrntBIM1ZLTwA38jYMQeHoeAaM12CHQ3ctMBBoijcwmyCIfGbUV2EHRSZmp5AoA9fGKT";
 
     #[test]
     fn generate_png_test() {
@@ -132,7 +146,7 @@ mod tests {
 
     #[test]
     fn generate_svg_test() {
-        let svg: String = make_svg_text(TEST_DATA);
+        let svg: String = make_svg_from_data(TEST_DATA);
         std::fs::write("og_example.svg", svg).unwrap();
     }
 

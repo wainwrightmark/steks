db99b9b24ab07b0b3695e3b430beed5142cd9eb8
Mark Wainwright <wainwrightml@gmail.com>
2023-06-01 17:42:14 +0100

    use quickmenu

--- src/menu.rs
+++ src/menu.rs
@@ -1,197 +1,56 @@
-use strum::Display;
-
 use crate::{
-    recording::{RecordEvent, RecordingState},
+    recording::RecordEvent,
+    set_level::set_levels_len,
     share::{ShareEvent, ShareSavedSvgEvent},
     *,
 };
-use ChangeLevelEvent;
-pub struct ButtonPlugin;
+use bevy::utils::HashMap;
+use bevy_quickmenu::{
+    style::{StyleEntry, Stylesheet},
+    *,
+};
 
-impl Plugin for ButtonPlugin {
+use itertools::Itertools;
+use ChangeLevelEvent;
+pub struct MenuPlugin;
+
+impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup.after(setup_level_ui))
-            .add_system(button_system.in_base_set(CoreSet::First));
+        let options = MenuOptions {
+            font: Some("fonts/FiraMono-Medium.ttf"),
+            ..Default::default()
+        };
+
+        app.add_event::<MenuEvent>()
+            .add_plugin(QuickMenuPlugin::<Screens>::with_options(options))
+            .add_startup_system(menu_setup)
+            .add_system(forward_events);
     }
 }
 
-#[derive(Component)]
-pub struct MainMenu;
+/// All possible screens in our example
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+enum Screens {
+    Root,
+    Level,
+}
 
-const NORMAL_BUTTON: Color = Color::Rgba {
-    red: 0.0,
-    green: 0.0,
-    blue: 0.0,
-    alpha: 0.0,
-}; //, green: (), blue: (), alpha: () } Color::rgb(0.9, 0.9, 0.9);
-
-const HOVERED_BUTTON: Color = Color::rgb(0.8, 0.8, 0.8);
-const PRESSED_BUTTON: Color = Color::rgb(0.7, 0.7, 0.7);
-
-const BUTTON_BACKGROUND: Color = Color::rgb(0.1, 0.1, 0.1);
-
-const BUTTON_WIDTH: f32 = 65.;
-const BUTTON_HEIGHT: f32 = 65.;
-
-fn button_system(
-    mut interaction_query: Query<
-        (&Interaction, &mut BackgroundColor, &MenuButton),
-        (Changed<Interaction>, With<Button>),
-    >,
-    mut change_level_events: EventWriter<ChangeLevelEvent>,
-    mut menu_query: Query<&mut Visibility, With<MainMenu>>,
-    mut share_saved_events: EventWriter<ShareSavedSvgEvent>,
-    mut share_events: EventWriter<ShareEvent>,
-    mut recording_events: EventWriter<RecordEvent>,
-) {
-    for (interaction, mut color, button) in interaction_query.iter_mut() {
-        use MenuButton::*;
-        //info!("{:?}", interaction);
-        match *interaction {
-            Interaction::Clicked => {
-                *color = PRESSED_BUTTON.into();
-                let mut menu_visibility = menu_query.single_mut();
-
-                //info!("{:?}", *button);
-                match *button {
-                    ToggleMenu => {
-                        *menu_visibility = match *menu_visibility {
-                            Visibility::Inherited => Visibility::Hidden,
-                            Visibility::Hidden => Visibility::Inherited,
-                            Visibility::Visible => Visibility::Hidden,
-                        }
-                    }
-                    GoFullscreen => {
-                        #[cfg(target_arch = "wasm32")]
-                        {
-                            crate::wasm::request_fullscreen();
-                        }
-                    }
-                    Tutorial => change_level_events.send(ChangeLevelEvent::StartTutorial),
-                    Infinite => change_level_events.send(ChangeLevelEvent::StartInfinite),
-                    DailyChallenge => change_level_events.send(ChangeLevelEvent::StartChallenge),
-                    ResetLevel => change_level_events.send(ChangeLevelEvent::ResetLevel),
-                    Share => share_events.send(ShareEvent),
-                    ShareSaved => share_saved_events.send(ShareSavedSvgEvent),
-
-                    StartRecording => recording_events.send(RecordEvent::Start),
-                    StopRecording => recording_events.send(RecordEvent::Stop),
-                }
-
-                if !matches!(*button, ToggleMenu) {
-                    *menu_visibility = Visibility::Hidden
-                }
-            }
-            Interaction::Hovered => {
-                *color = HOVERED_BUTTON.into();
-            }
-            Interaction::None => {
-                *color = NORMAL_BUTTON.into();
-            }
+/// Map from from `Screens` to the actual menu
+impl ScreenTrait for Screens {
+    type Action = MenuAction;
+    type State = SteksMenuState;
+    fn resolve(&self, state: &SteksMenuState) -> Menu<Screens> {
+        match self {
+            Screens::Root => root_menu(state),
+            Screens::Level => level_menu(state),
         }
     }
 }
 
-fn spawn_menu(commands: &mut Commands, asset_server: &AssetServer) {
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                position_type: PositionType::Absolute,
-                position: UiRect {
-                    left: Val::Px(10.),
-                    top: Val::Px(10. + BUTTON_HEIGHT),
-                    ..Default::default()
-                },
-                display: Display::Flex,
-                flex_direction: FlexDirection::Column,
-                ..Default::default()
-            },
-            z_index: ZIndex::Global(10),
-            visibility: Visibility::Hidden,
-            ..Default::default()
-        })
-        .insert(MainMenu)
-        .with_children(|parent| {
-            use MenuButton::*;
-            for button in [
-                // ToggleMenu,
-                ResetLevel,
-                #[cfg(target_arch = "wasm32")]
-                GoFullscreen,
-                Tutorial,
-                Infinite,
-                DailyChallenge,
-                #[cfg(target_arch = "wasm32")]
-                Share,
-                #[cfg(target_arch = "wasm32")]
-                StartRecording
-            ] {
-                spawn_button(parent, button, asset_server);
-            }
-        });
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                position_type: PositionType::Absolute,
-                position: UiRect {
-                    left: Val::Px(10.),
-                    top: Val::Px(10.),
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            z_index: ZIndex::Global(10),
-            ..Default::default()
-        })
-        .with_children(|parent| {
-            spawn_button(parent, MenuButton::ToggleMenu, asset_server.as_ref())
-        });
-
-    spawn_menu(&mut commands, asset_server.as_ref())
-}
-
-pub fn spawn_button(
-    parent: &mut ChildBuilder,
-    menu_button: MenuButton,
-    asset_server: &AssetServer,
-) {
-    parent
-        .spawn(ButtonBundle {
-            style: Style {
-                size: Size::new(Val::Px(BUTTON_WIDTH), Val::Px(BUTTON_HEIGHT)),
-                margin: UiRect::all(Val::Auto),
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                flex_grow: 0.0,
-                flex_shrink: 0.0,
-
-                ..Default::default()
-            },
-            background_color: NORMAL_BUTTON.into(),
-            ..Default::default()
-        })
-        .with_children(|parent| {
-            parent.spawn(TextBundle {
-                text: Text::from_section(
-                    menu_button.text(),
-                    TextStyle {
-                        font: asset_server.load("fonts/fontello-font.ttf"),
-                        font_size: 30.0,
-                        color: BUTTON_BACKGROUND,
-                    },
-                ),
-                ..Default::default()
-            });
-        })
-        .insert(menu_button);
-}
-
-#[derive(Component, Clone, Copy, Debug, Display, PartialEq, Eq)]
-pub enum MenuButton {
-    ToggleMenu,
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum MenuAction {
+    OpenMenu,
+    CloseMenu,
     ResetLevel,
     GoFullscreen,
     Tutorial,
@@ -201,22 +60,211 @@ pub enum MenuButton {
     Share,
     StartRecording,
     StopRecording,
+    SelectLevel(u8),
 }
 
-impl MenuButton {
-    pub fn text(&self) -> &'static str {
-        use MenuButton::*;
+impl MenuAction {
+    // pub fn to_icon_name(&self) -> &'static str {
+    //     match self {
+    //         MenuAction::OpenMenu => "menu",
+    //         MenuAction::CloseMenu => "minus",
+    //         MenuAction::ResetLevel => "arrows-cw",
+    //         MenuAction::GoFullscreen => "resize-full-alt",
+    //         MenuAction::Tutorial => "lightbulb",
+    //         MenuAction::Infinite => "infinity",
+    //         MenuAction::DailyChallenge => "award",
+    //         MenuAction::ShareSaved => "share",
+    //         MenuAction::Share => "share",
+    //         MenuAction::StartRecording => "record",
+    //         MenuAction::StopRecording => "stop",
+    //         MenuAction::SelectLevel(_) => "th",
+    //     }
+    // }
+
+    fn name(&self)-> &'static str{
+        match self{
+            MenuAction::OpenMenu => "Menu",
+            MenuAction::CloseMenu => "Menu",
+            MenuAction::ResetLevel => "Reset",
+            MenuAction::GoFullscreen => "Fullscreen",
+            MenuAction::Tutorial => "Tutorial",
+            MenuAction::Infinite => "Infinite",
+            MenuAction::DailyChallenge => "Challenge",
+            MenuAction::ShareSaved => "Share",
+            MenuAction::Share => "Share",
+            MenuAction::StartRecording => "Start",
+            MenuAction::StopRecording => "Stop",
+            MenuAction::SelectLevel(_) => "Level",
+        }
+    }
+
+    fn to_menu_item(&self, state: &SteksMenuState) -> MenuItem<Screens> {
+        MenuItem::action(self.name(), self.clone())
+    }
+}
+
+fn level_menu(_state: &SteksMenuState) -> Menu<Screens> {
+    let levels = (0..set_levels_len())
+        .map(|x| MenuItem::action(x.to_string(), MenuAction::SelectLevel(x as u8)))
+        .collect_vec();
+
+    Menu::new("levels", levels)
+}
+
+/// The `root` menu that is displayed first
+fn root_menu(state: &SteksMenuState) -> Menu<Screens> {
+    if state.open {
+        Menu::new(
+            "root-open",
+            vec![
+                MenuAction::CloseMenu.to_menu_item(state),
+                MenuAction::ResetLevel.to_menu_item(state),
+                MenuAction::GoFullscreen.to_menu_item(state),
+                MenuAction::Tutorial.to_menu_item(state),
+                MenuAction::Infinite.to_menu_item(state),
+                MenuAction::DailyChallenge.to_menu_item(state),
+                MenuAction::Share.to_menu_item(state),
+                if state.recording {
+                    MenuAction::StopRecording.to_menu_item(state)
+                } else {
+                    MenuAction::StartRecording.to_menu_item(state)
+                },
+                MenuItem::screen("Levels", Screens::Level),
+
+            ],
+        )
+    } else {
+        Menu::new(
+            "root-closed",
+            vec![MenuAction::OpenMenu.to_menu_item(state)],
+        )
+    }
+}
+
+fn menu_setup(mut commands: Commands) {
+    let mut button = StyleEntry::button();
+
+    button.normal.fg = Color::BLACK;
+    button.normal.bg = Color::NONE;
+    button.hover.fg = Color::BLACK;
+    button.hover.bg = Color::GRAY;
+
+    button.selected = button.normal;
+
+    let sheet = Stylesheet {
+        button,
+        label: StyleEntry::label(),
+        headline: StyleEntry::headline(),
+        vertical_spacing: 0f32,
+        style: Default::default(),
+        background: Default::default(),
+    };
+
+
+    let state = SteksMenuState {
+        open: false,
+        recording: false,
+    };
+
+    commands.insert_resource(MenuState::new(state, Screens::Root, Some(sheet)))
+}
+
+#[derive(Component)]
+pub struct MainMenu;
+
+#[derive(Debug, Clone)]
+pub enum MenuEvent {
+    ChangeLevel(ChangeLevelEvent),
+    ShareSaved(ShareSavedSvgEvent),
+    Share(ShareEvent),
+    Record(RecordEvent),
+}
+
+impl From<ChangeLevelEvent> for MenuEvent {
+    fn from(value: ChangeLevelEvent) -> Self {
+        Self::ChangeLevel(value)
+    }
+}
+impl From<ShareSavedSvgEvent> for MenuEvent {
+    fn from(value: ShareSavedSvgEvent) -> Self {
+        Self::ShareSaved(value)
+    }
+}
+impl From<ShareEvent> for MenuEvent {
+    fn from(value: ShareEvent) -> Self {
+        Self::Share(value)
+    }
+}
+impl From<RecordEvent> for MenuEvent {
+    fn from(value: RecordEvent) -> Self {
+        Self::Record(value)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Resource)]
+pub struct SteksMenuState {
+    pub open: bool,
+    pub recording: bool,
+}
+
+
+impl ActionTrait for MenuAction {
+    type State = SteksMenuState;
+    type Event = MenuEvent;
+
+    fn handle(&self, state: &mut Self::State, event_writer: &mut EventWriter<Self::Event>) {
         match self {
-            ToggleMenu => "\u{f0c9}",     // "Menu",
-            ResetLevel => "\u{e800}",     //"Reset Level",image
-            GoFullscreen => "\u{f0b2}",   //"Fullscreen",
-            Tutorial => "\u{e801}",       //"Tutorial",
-            Infinite => "\u{e802}",       //"Infinite",
-            DailyChallenge => "\u{e803}", // "Challenge",
-            Share => "\u{f1e0}",          // "Share",
-            ShareSaved => "\u{f1e0}",     // "Share",
-            StartRecording => "\u{e807}",
-            StopRecording => "\u{e809}",
+            MenuAction::GoFullscreen => {
+                #[cfg(target_arch = "wasm32")]
+                {
+                    crate::wasm::request_fullscreen();
+                }
+            }
+            MenuAction::Tutorial => event_writer.send(ChangeLevelEvent::StartTutorial.into()),
+            MenuAction::Infinite => event_writer.send(ChangeLevelEvent::StartInfinite.into()),
+            MenuAction::DailyChallenge => {
+                event_writer.send(ChangeLevelEvent::StartChallenge.into())
+            }
+            MenuAction::ResetLevel => event_writer.send(ChangeLevelEvent::ResetLevel.into()),
+            MenuAction::Share => event_writer.send(ShareEvent.into()),
+            MenuAction::ShareSaved => event_writer.send(ShareSavedSvgEvent.into()),
+
+            MenuAction::StartRecording => {
+                event_writer.send(RecordEvent::Start.into());
+                state.recording = true;
+            }
+            MenuAction::StopRecording => {
+                event_writer.send(RecordEvent::Stop.into());
+                state.recording = false;
+            }
+            MenuAction::OpenMenu => {
+                state.open = true;
+                return;
+            }
+            MenuAction::CloseMenu => {}
+            MenuAction::SelectLevel(l) => {
+                event_writer.send(ChangeLevelEvent::ChooseLevel(*l).into());
+                state.open = false;
+            }
+        }
+
+        state.open = false;
+    }
+}
+
+fn forward_events(
+    mut menu_events: EventReader<MenuEvent>,
+    mut change_level_events: EventWriter<ChangeLevelEvent>,
+    mut share_saved_events: EventWriter<ShareSavedSvgEvent>,
+    mut share_events: EventWriter<ShareEvent>,
+    mut recording_events: EventWriter<RecordEvent>,
+) {
+    for ev in menu_events.into_iter() {
+        match ev.clone() {
+            MenuEvent::ChangeLevel(x) => change_level_events.send(x),
+            MenuEvent::ShareSaved(x) => share_saved_events.send(x),
+            MenuEvent::Share(x) => share_events.send(x),
+            MenuEvent::Record(x) => recording_events.send(x),
         }
     }
 }

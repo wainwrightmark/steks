0f07192666b612afcc60bd5025746cdf8e591e97
Mark Wainwright <wainwrightml@gmail.com>
2023-06-09 15:50:43 +0100

    fixed compilation error

--- src/leaderboard.rs
+++ src/leaderboard.rs
@@ -1,15 +1,22 @@
 use std::ops::Mul;
 
+use bevy::prelude::Plugin;
 use itertools::Itertools;
 
 use crate::game_shape::GameShape;
 
 pub struct LeaderboardPlugin;
 
-pub fn hash_shapes(shapes: impl Iterator<Item = GameShape>)-> u64{
-    let mut code = 0;
+impl Plugin for LeaderboardPlugin{
+    fn build(&self, app: &mut bevy::prelude::App) {
+
+    }
+}
+
+pub fn hash_shapes(shapes: impl Iterator<Item = GameShape>)-> i64{
+    let mut code: i64 = 0;
     for index in shapes.map(|x|x.index).sorted(){
-        code.wrapping_mul(31)
+        code =  code.wrapping_mul(31).wrapping_add(index as i64);
     }
 
     code

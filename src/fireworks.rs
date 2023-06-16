cba67178f1d889541bdf6e72ebd98b20ddb71e3d
Mark Wainwright <wainwrightml@gmail.com>
2023-06-16 18:40:43 +0100

    improved fireworks

--- src/fireworks.rs
+++ src/fireworks.rs
@@ -1,4 +1,6 @@
-use bevy::prelude::*;
+use std::num;
+
+use bevy::{prelude::*, window::PrimaryWindow};
 use bevy_prototype_lyon::prelude::ShapeBundle;
 use bevy_rapier2d::prelude::{Collider, CollisionGroups, GravityScale, Group, RigidBody, Velocity};
 use rand::{rngs::ThreadRng, seq::SliceRandom, Rng};
@@ -23,8 +25,14 @@ impl Plugin for FireworksPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_system(spawn_fireworks)
             .add_system(despawn_fireworks)
+            .add_system(queue_fireworks)
             .init_resource::<FireworksDespawnTimer>();
     }
+}
+
+#[derive(Debug, Component)]
+struct SpawnFireWorksTimer {
+    timer: Timer,
 }
 
 fn despawn_fireworks(
@@ -36,7 +44,7 @@ fn despawn_fireworks(
     if let Some(timer) = timer_resource.timer.as_mut() {
         timer.tick(time.delta());
 
-        if timer.finished() {
+        if timer.just_finished() {
             timer_resource.timer = None;
             for firework in fireworks.iter() {
                 commands.entity(firework).despawn();
@@ -46,6 +54,35 @@ fn despawn_fireworks(
 }
 
 fn spawn_fireworks(
+    mut commands: Commands,
+
+    mut queue: Query<(Entity, &mut SpawnFireWorksTimer)>,
+    time: Res<Time>,
+    window: Query<&Window, With<PrimaryWindow>>,
+) {
+    for (entity, mut timer) in queue.iter_mut() {
+        timer.timer.tick(time.delta());
+
+        if timer.timer.just_finished() {
+            commands.entity(entity).despawn();
+
+            let mut rng: ThreadRng = rand::thread_rng();
+
+            let window = window.get_single().unwrap();
+
+            let sparks = rng.gen_range(SPARKS_MIN..=SPARKS_MAX);
+
+            let x = rng.gen_range((window.width() * -0.5)..=(window.width() * 0.5));
+            let y = rng.gen_range(0.0..=(window.height() * 0.5));
+            let translation = Vec2 { x, y }.extend(0.0);
+            for _ in 0..sparks {
+                spawn_spark(&mut commands, translation, &mut rng);
+            }
+        }
+    }
+}
+
+fn queue_fireworks(
     mut commands: Commands,
     current_level: Res<CurrentLevel>,
     mut previous: Local<CurrentLevel>,
@@ -77,27 +114,39 @@ fn spawn_fireworks(
         return;
     }
 
+    timer_resource.timer = Some(Timer::from_seconds(FIREWORK_SECONDS, TimerMode::Once));
+
     let mut rng: ThreadRng = rand::thread_rng();
 
-    for _ in 0..NUMBER_OF_FIREWORKS {
-        spawn_firework(&mut commands, &mut rng);
+    for _ in 0..NUMBER_OF_EXPLOSIONS{
+        let delay = rng.gen_range(0.0..2.0);
+        commands.spawn(SpawnFireWorksTimer{
+            timer: Timer::from_seconds(delay, TimerMode::Once)
+        });
     }
-
-    timer_resource.timer = Some(Timer::from_seconds(FIREWORK_SECONDS, TimerMode::Once));
 }
 
-const NUMBER_OF_FIREWORKS: usize = 50;
+const NUMBER_OF_EXPLOSIONS: usize = 10;
 
-const FIREWORK_SECONDS: f32 = 3.0;
+const SPARKS_MIN: usize = 20;
+const SPARKS_MAX: usize = 50;
+
+const FIREWORK_SECONDS: f32 = 8.0;
 
 const FIREWORK_SIZE: f32 = 10.0;
 
-fn spawn_firework<R: Rng>(commands: &mut Commands, rng: &mut R) {
+// const MIN_X: f32 = -300.0;
+// const MAX_X: f32 = 300.0;
+
+// const MIN_Y: f32 = 00.0;
+// const MAX_Y: f32 = 300.0;
+
+fn spawn_spark<R: Rng>(commands: &mut Commands, translation: Vec3, rng: &mut R) {
     let game_shape = game_shape::ALL_SHAPES.choose(rng).unwrap();
     let shape_bundle = game_shape.body.get_shape_bundle(FIREWORK_SIZE);
     let angvel = rng.gen_range(-1.0..1.0);
     let x = rng.gen_range(-200.0..200.0);
-    let y = rng.gen_range(500.0..1000.0);
+    let y = rng.gen_range(-500.0..500.0);
 
     let velocity: Velocity = Velocity {
         linvel: Vec2 { x, y },
@@ -125,5 +174,5 @@ fn spawn_firework<R: Rng>(commands: &mut Commands, rng: &mut R) {
         .insert(GravityScale::default())
         .insert(velocity)
         .insert(Firework)
-        .insert(Transform::default());
+        .insert(Transform::from_translation(translation));
 }

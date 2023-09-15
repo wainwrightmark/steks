8c49ff9770a9375d10304848ea8f5298917954a2
Mark Wainwright <wainwrightml@gmail.com>
2023-09-15 16:48:34 +0100

    added zlib compression and base91 to ad postbuild

--- ad_postbuild/src/main.rs
+++ ad_postbuild/src/main.rs
@@ -1,10 +1,8 @@
-use base64::{engine::general_purpose, Engine as _};
+use regex::Regex;
 use std::env;
 use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
-use regex::Regex;
-
 
 fn main() -> Result<(), anyhow::Error> {
     let staging_dir = env::var("TRUNK_STAGING_DIR")?;
@@ -44,43 +42,61 @@ fn main() -> Result<(), anyhow::Error> {
     let wasm_data = fs::read(wasm_file_path.clone())?;
 
     println!("Wasm data is {} bytes", wasm_data.len());
-    let encoded_wasm: String = general_purpose::STANDARD_NO_PAD.encode(wasm_data);
+
+    let compressed_wasm = miniz_oxide::deflate::compress_to_vec(&wasm_data, 10);
+    let encoded_wasm_bytes = base91::slice_encode(&compressed_wasm);
+
+    let encoded_wasm = String::from_utf8_lossy(&encoded_wasm_bytes);
+
+    println!(
+        "Wasm data encodes to {} base91 chars",
+        encoded_wasm.chars().count()
+    );
 
     let contents = "const data = '".to_string()
-        + encoded_wasm.as_str()
-        + "';\n"
-        + r#"
-    function get_data(){
-        var binary_string = window.atob(data);
-        var len = binary_string.length;
-        var bytes = new Uint8Array(len);
-        for (var i = 0; i < len; i++) {
-            bytes[i] = binary_string.charCodeAt(i);
-        }
-        return bytes.buffer;
-    }
-
-    export default get_data;
-    "#;
+        + encoded_wasm.to_string().as_str()
+        + "';\n export default data";
 
     fs::write(staging_dir.join("encoded_wasm.js"), contents)?;
 
+    fs::write(
+        staging_dir.join("pako_inflate.js"),
+        include_bytes!("pako_inflate.js"),
+    )?;
+
     fs::remove_file(wasm_file_path)?;
-    let js_file_name = js_file_path.file_name().expect("js file should have name").to_str().unwrap();
+    let js_file_name = js_file_path
+        .file_name()
+        .expect("js file should have name")
+        .to_str()
+        .unwrap();
 
-    let html_text =  fs::read_to_string(html_file_path.clone())?;
+    let html_text = fs::read_to_string(html_file_path.clone())?;
 
-    let regex1 = Regex::new(r#"<link rel="preload" href="/steks_ad-[a-z0-9]+_bg\.wasm" as="fetch" type="application/wasm" crossorigin="">"#)?;
+    let regex1 = Regex::new(
+        r#"<link rel="preload" href="/steks_ad-[a-z0-9]+_bg\.wasm" as="fetch" type="application/wasm" crossorigin="">"#,
+    )?;
     let rep1 = r#"<link rel="modulepreload" href="./encoded_wasm.js"></head>"#;
-    let html_text= regex1.replace(&html_text, rep1);
+    let html_text = regex1.replace(&html_text, rep1);
 
-    let regex2 = Regex::new(r#"<script type="module">import init from '/steks_ad-[a-z0-9]+\.js';init\('/steks_ad-[a-z0-9]+_bg.wasm'\);</script>"#)?;
-    let rep2 : String = r#"<script type="module">
+    let regex2 = Regex::new(
+        r#"<script type="module">import init from '/steks_ad-[a-z0-9]+\.js';init\('/steks_ad-[a-z0-9]+_bg.wasm'\);</script>"#,
+    )?;
+
+    let base_91_js = include_str!("base91.js");
+    let rep2: String = r#"
+    <script type="text/javascript" src="pako_inflate.js"></script>
+    <script type="module">
     import data from "./encoded_wasm.js"
-    import init from './"#.to_string() + js_file_name +
-    r#"';
-    const array_buffer = data();
-    init(array_buffer);
+
+    import init from './"#
+        .to_string()
+        + js_file_name
+        + r#"';"#
+        + base_91_js
+        + r#"const decoded = decode(data);
+        const inflated = pako.inflateRaw(decoded);
+          init(inflated);
     </script>
     "#;
     let html_text = regex2.replace(&html_text, rep2);
